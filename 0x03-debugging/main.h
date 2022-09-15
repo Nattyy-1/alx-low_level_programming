41c00993808168c28f9abb0bba126b7fbd411af9
Natnael Samson <Natnaelsamson41@gmail.com>
2022-09-15 03:38:49 -0700

    fix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,24 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- *
- * Desc: Header file containing declartions for all function
- */
+#include <stdio.h>
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void time_teble(void);
-int add(int, int);
-void print_to_98(int n);
-void print_time_table(int n);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-#endif
+#endif /* MAIN_H */
