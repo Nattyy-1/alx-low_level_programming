ae3639215e5ccd7d184166bfa9159b4d1853aad8
Natnael Samson <Natnaelsamson41@gmail.com>
2025-06-14 18:10:45 +0300

    Add answer for question 5

---
+++ 0x01-variables_if_else_while/4-print_alpphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if success, otherwise non-zero
+ */
+int main(void)
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
