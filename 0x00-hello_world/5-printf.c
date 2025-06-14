18796ef9a3c5c1eae05d3c349a8d985b1b9f37c1
Natnael Samson <Natnaelsamson41@gmail.com>
2025-06-14 12:13:32 +0300

    Add answer for question 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if success, otherwise non-zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n"
+	return (0);
+}
