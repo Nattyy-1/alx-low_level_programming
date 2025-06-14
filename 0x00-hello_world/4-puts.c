f0b2331abca650b0c781c7013a8c09f3a575079c
Natnael Samson <Natnaelsamson41@gmail.com>
2025-06-14 12:08:56 +0300

    Add answer for question 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: 0 if success, otherwise non-zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
