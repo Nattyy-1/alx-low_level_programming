5f3ab24da31f5612ee17c64f31dc09a2aac18512
Natnael Samson <Natnaelsamson41@gmail.com>
2025-06-14 18:52:41 +0300

    Add answer for question 10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if success, otherwise non-zero
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
