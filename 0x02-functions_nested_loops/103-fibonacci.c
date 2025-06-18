ea572fb0d39a3f4b574dee4e1310d1b7c3b95a2d
Natnael Samson <Natnaelsamson41@gmail.com>
2025-06-18 21:34:25 +0300

    Fix answer for question 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 	unsigned long int num1 = 1;
 	unsigned long int num2 = 2;
 	unsigned long int fib = num1 + num2;
-	unsigned long int sum = 0;
+	unsigned long int sum = 2;
 	unsigned long int temp;
 
 	while (fib < 4000000)
