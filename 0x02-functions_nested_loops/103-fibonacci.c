fc44982789fd22fe253c1218c63369a17d151897
Natnael Samson <Natnaelsamson41@gmail.com>
2025-06-18 21:32:27 +0300

    Add answer for question number 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if success, otherwise non-zero
+ */
+int main(void)
+{
+	unsigned long int num1 = 1;
+	unsigned long int num2 = 2;
+	unsigned long int fib = num1 + num2;
+	unsigned long int sum = 0;
+	unsigned long int temp;
+
+	while (fib < 4000000)
+	{
+		if (fib % 2 == 0)
+			sum += fib;
+
+		temp = fib;
+
+		fib += num2;
+		num2 = temp;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
