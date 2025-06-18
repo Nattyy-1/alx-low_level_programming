bc9cd2131d0d7bd515910a4a1c5d2914c519f7bd
Natnael Samson <Natnaelsamson41@gmail.com>
2025-06-18 20:59:42 +0300

    Add answer for question 14

---
+++ 0x02-functions_nested_loops/102-fibonnaci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 if success, otherwise non-zero
+ */
+int main(void)
+{
+	int num1 = 1;
+	long int num2 = 2;
+	long int sum = num1 + num2;
+
+	printf("%d, %lu, %lu, ", num1, num2, sum);
+
+	int n = 0;
+
+	while (n < 48)
+	{
+		int temp = sum;
+
+		sum += num2;
+		num2 = temp;
+		printf("%lu", sum);
+
+		if (n != 47)
+			printf(", ");
+		n++;
+	}
+}
