cd787fa0c7cfe3424624c4d69d06e82c894565e7
Natnael Samson <Natnaelsamson41@gmail.com>
2025-06-18 19:52:31 +0300

    Add answer for question 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#inlcude "main.h"
+
+/**
+ * add - adds the given two numbers
+ * @n: the first digit to be added
+ * @m: the second digit to be aded
+ *
+ * Return: the sum of two digits
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
