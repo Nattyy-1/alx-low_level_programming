2e7f397effb2eb358f707855f450fe8bb70bb8bb
Natnael Samson <Natnaelsamson41@gmail.com>
2025-07-07 11:19:39 +0300

    Add answer for question 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - gives the length of a string
+ * @s: pointer to the string being checked
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+}
