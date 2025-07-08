5f59ec61706646852fbaede5d816ec083e69bf48
Natnael Samson <Natnaelsamson41@gmail.com>
2025-07-08 18:26:53 +0300

    fix answer for question 4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: first string to be compared
  * @s2: second string to be compared
  *
- * Return: 1 if the strings are equal, 0 otherwise
+ * Return: positive if s1 > s2, negative if s1 < s2, 0 otherwise
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -13,7 +13,6 @@ int _strcmp(char *s1, char *s2)
 	int s1_len = 0;
 	int s2_len = 0;
 	int i;
-	int bool = 1;
 
 	while (s1[s1_len] != '\0')
 		s1_len++;
@@ -21,8 +20,11 @@ int _strcmp(char *s1, char *s2)
 	while (s2[s2_len] != '\0')
 		s2_len++;
 
-	if (s1_len != s2_len)
-		return (0);
+	if (s1_len <  s2_len)
+		return (-1);
+
+	if (s1_len > s2_len)
+		return (1);
 
 	for (i = 0; i < s1_len; i++)
 	{
@@ -31,13 +33,9 @@ int _strcmp(char *s1, char *s2)
 			continue;
 		} else
 		{
-			bool = -1;
-			break;
+			return (s1[i] - s2[i]);
 		}
 	}
 
-	if (bool != 1)
-		return (0);
-	else
-		return (1);
+	return (0);
 }
