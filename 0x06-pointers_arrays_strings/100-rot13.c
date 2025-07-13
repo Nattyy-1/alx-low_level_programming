ec6044a1790ce991896d8970ec05c7babb5d912c
Natnael Samson <Natnaelsamson41@gmail.com>
2025-07-13 13:23:08 +0300

    Add answer for question 9

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s : the string being manipulated
+ *
+ * Return: pontinter to the string that has been changed
+ */
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' || s[i] >= 'A'))
+			s[i] = s[i + 13};
+	}
+
+	return (s);
+}
