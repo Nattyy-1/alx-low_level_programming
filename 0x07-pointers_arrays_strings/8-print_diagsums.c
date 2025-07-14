abe8e0197a8c7635520ae93ccaeaf33def234319
Natnael Samson <Natnaelsamson41@gmail.com>
2025-07-14 12:42:51 +0300

    Fix answer for question 9

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the usm of the two diagonals of a square matrix
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
  * @a: the 2d array that is being manipulated
  * @size: size of the 2d array
  */
@@ -13,11 +13,11 @@ void print_diagsums(int *a, int size)
 	int right_diag_sum = 0;
 	int left_diag_sum = 0;
 
-	for (i = 0; i < size; i++)
-		right_diag_sum += a[i][j];
+	for (i = 0, j = 0; i < size; i++)
+		right_diag_sum += a[i * size + j];
 
 	for (i = 0, j = size - 1; i < size; i++, j--)
-		left_diag_sum += a[i][j];
+		left_diag_sum += a[i * size + j];
 
-	printf("%d, %d/n", right_diag_sum, left_diag_sum);
+	printf("%d, %d\n", right_diag_sum, left_diag_sum);
 }
