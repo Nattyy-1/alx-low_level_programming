2bfd45de76fe81577668c916502aaab6400ab06c
Natnael Samson <Natnaelsamson41@gmail.com>
2025-07-17 13:51:52 +0300

    fix answer for question 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints the number of arguments passed to it
  * @argc: the number of arguments
  * @argv: the arguments passed to main
  *
- * Return: 0 if success, otherwise none zero
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
