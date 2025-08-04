624ad4bd962ec8482347d527ce895d8795913189
Natnael Samson <Natnaelsamson41@gmail.com>
2025-08-04 18:18:07 +0300

    Add answer for question 3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: 0 if success, otherwise non zero
+ */
+int main(void)
+{
+	printf("%S\n", __FILE__);
+	return (0);
+}
