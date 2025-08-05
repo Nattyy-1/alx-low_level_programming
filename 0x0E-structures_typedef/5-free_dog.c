fe345cb66084f0fd586ac83b5cd0b825ffc4e361
Natnael Samson <Natnaelsamson41@gmail.com>
2025-08-05 19:15:59 +0300

    Add answer for question 6

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a dog variable
+ * @d: the dog variable that needs to be freed
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
