a43ef67ecefa1453689e3d06ce95bbdbef184775
Natnael Samson <Natnaelsamson41@gmail.com>
2025-08-05 18:02:57 +0300

    Add answer for question 1

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This contains answers related to Structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure to hold data on a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: name of owner of the dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
