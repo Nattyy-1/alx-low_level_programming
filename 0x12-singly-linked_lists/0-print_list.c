a9b659db154d204789639d80f38492d589c8682f
Natnael Samson <Natnaelsamson41@gmail.com>
2025-08-31 12:34:02 +0300

    Fix requirement issue

--- 0x12-singly-linked_lists/0-print_list.c
+++ 0x12-singly-linked_lists/0-print_list.c
@@ -1,29 +1,5 @@
 #include "lists.h"
-
-/**
- * print_list_helper - prints a give list of type list_t
- * @h: head of the linked list
- * @node_count: the number of nodes encountered so far
- *
- * Return: the number of nodes in the list
- */
-size_t print_list_helper(const list_t *h, size_t *node_count)
-{
-	if (h == NULL)
-	{
-		return (*node_count);
-	}
-
-	(*node_count)++;
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	} else
-	{
-		printf("[%d] %s\n", h->len, h->str);
-	}
-	return (print_list_helper(h->next, node_count));
-}
+#include "print_list_helper.c"
 
 /**
  * print_list - prints all the elements of a list_t list
