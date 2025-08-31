7a5c2db6360520f2febbafe6f230fe463eceebcc
Natnael Samson <Natnaelsamson41@gmail.com>
2025-08-31 20:27:56 +0300

    Fix 2 for question 6 answer

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL)
 		return;
 
-	tmp = (*head)->next;
-	free(*head);
+	while (*head != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+
 	*head = NULL;
-	free_listint2(&tmp);
 }
+
