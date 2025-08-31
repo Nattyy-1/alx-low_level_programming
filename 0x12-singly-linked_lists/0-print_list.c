410759088c4a294ce6d2da16d5bdaab72e8534d4
Natnael Samson <Natnaelsamson41@gmail.com>
2025-08-31 12:15:32 +0300

    Fix missing helper file issue

--- 0x12-singly-linked_lists/0-print_list.c
+++ 0x12-singly-linked_lists/0-print_list.c
@@ -1,6 +1,31 @@
 #include "lists.h"
 
 /**
+ * print_list_helper - prints a give list of type list_t
+ * @h: head of the linked list
+ * @node_count: the number of nodes encountered so far
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_list_helper(const list_t *h, size_t *node_count)
+{
+	if (h == NULL)
+	{
+		return (*node_count);
+	}
+
+	(*node_count)++;
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	} else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+	}
+	return (print_list_helper(h->next, node_count));
+}
+
+/**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
  *
@@ -13,3 +38,4 @@ size_t print_list(const list_t *h)
 	print_list_helper(h, &node_count);
 	return (node_count);
 }
+
