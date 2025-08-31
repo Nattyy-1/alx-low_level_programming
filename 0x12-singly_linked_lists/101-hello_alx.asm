c680ccb937013cca2d4b667575540ce1bc2988a8
Natnael Samson <Natnaelsamson41@gmail.com>
2025-08-31 17:37:48 +0300

    Fix answer for question 7

--- 0x12-singly_linked_lists/101-hello_alx.asm
+++ 0x12-singly_linked_lists/101-hello_alx.asm
@@ -1,15 +1,15 @@
 global main
+extern printf
+
 section .data
-hello_world:
-    db "Hello, Alx", 0xA, 0
+msg:
+    db "Hello, ALX", 0xA, 0
 
 section .text
 main:
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rel hello_world]
-    mov rdx, 11
-    syscall
+    mov rdi, msg
+    xor rax, rax
+    call printf
     xor rax, rax
     ret
 
