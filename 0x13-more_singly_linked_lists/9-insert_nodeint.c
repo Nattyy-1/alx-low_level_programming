6e0ac9e00056ee50c57db39afbe6567084fb0de5
Natnael Samson <Natnaelsamson41@gmail.com>
2025-09-01 13:42:54 +0300

    fix answer for question 10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,48 +7,36 @@
  * @n: the data to be inserted in the new node
  *
  * Return: address of the new node if successful, NULL if it failed or it
- *	   is not possible to add the node at that idx
+ *     is not possible to add the node at that idx
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int node_count = 1;
 	listint_t *new_node, *temp;
+	unsigned int i;
 
 	if (head == NULL)
 		return (NULL);
-	if (*head == NULL)
+	if (idx == 0)
 	{
-		if (idx == node_count - 1)
-		{
-			new_node = malloc(sizeof(listint_t));
-			if (new_node == NULL)
-				return (NULL);
-
-			new_node->n = n;
-			new_node->next = NULL;
-			*head = new_node;
-			return (new_node);
-		}
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 	temp = *head;
-	while (temp != NULL && node_count <= idx)
-	{
-		if (node_count != idx)
-		{
-			node_count++;
-			temp = temp->next;
-			continue;
-		} else
-		{
-			new_node = malloc(sizeof(listint_t));
-			if (new_node == NULL)
-				return (NULL);
-
-			new_node->n = n;
-			new_node->next = (temp)->next;
-			(temp)->next = new_node;
-			return (new_node);
-		}
-	}
-	return (NULL);
+	for (i = 0; temp != NULL && i < idx - 1; i++)
+		temp = temp->next;
+	if (temp == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }
+
