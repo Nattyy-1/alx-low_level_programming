6644fa83e120520ec459284de8f3c863f00a72f9
Natnael Samson <Natnaelsamson41@gmail.com>
2025-09-14 12:23:52 +0300

    Add answer for question 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: 0 if succesful, otherwise non-zero
+ */
+int main(void)
+{
+	int n = 0;
+	char hex = 'a';
+
+	while (n < 16)
+	{
+		if (n >= 10)
+		{
+			putchar(hex);
+			hex++;
+			n++;
+		}
+		else
+		{
+			putchar(n + '0');
+			n++;
+		}
+	}
+	
+	putchar('\n');
+	return (0);
+}
