d1b877e8942bd2ba6aea5dfe86ac008da2fbddde
Natnael Samson <Natnaelsamson41@gmail.com>
2026-02-17 12:46:45 +0300

    Add answer for question 6

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * hash_table_print - prints a hash table
+ *
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current_node;
+	int first = 1;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current_node = ht->array[i];
+		while (current_node)
+		{
+			if (!first)
+				printf(", ");
+
+			printf("'%s': '%s'", current_node->key, current_node->value);
+			first = 0;
+
+			current_node = current_node->next;
+		}
+	}
+
+	printf("}\n");
+}
+
